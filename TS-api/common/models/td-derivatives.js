755ec819d93e4b1e57a3fea4cd4a447eae602f68
kishangopal <kishangopal.yuvmedia@gmail.com>
2023-09-30 10:02:29 +0530

    logn registe

--- TS-api/common/models/td-derivatives.js
+++ TS-api/common/models/td-derivatives.js
@@ -1,22 +1,200 @@
 "use strict";
 const request = require("request");
+const configt = require("../../server/config.json");
 const app = require("../../server/server");
 const _ = require("lodash");
+const moment = require("moment");
 module.exports = function (TdDerivatives) {
-  TdDerivatives.derivativestype = (type, callback) => {
-    const currenturl = `http://nimblerest.lisuns.com:4531/GetLastQuote/?accessKey=988dcf72-de6b-4637-9af7-fddbe9bfa7cd&exchange=NFO&instrumentIdentifier=${type}`;
-    console.log(currenturl);
+  TdDerivatives.strikeprice = (type, callback) => {
+    const currenturl = `${configt.stock.connector}/GetLastQuote/?accessKey=${configt.stock.key}&exchange=NFO&instrumentIdentifier=${type}`;
     request(currenturl, function (error, response, body) {
       if (!error && response.statusCode == 200) {
-        console.log(body);
+        const jsonData = JSON.parse(body);
         if (_.isEmpty(body)) {
-          callback(null, { list: { status: "0", message: "Data not find" } });
+          callback(null, {
+            StrikePrice: { status: "0", message: "Data not find", value: 0 },
+          });
         } else {
-          callback(null, { list: body });
+          callback(null, {
+            StrikePrice: {
+              status: "1",
+              message: "success",
+              value: jsonData.AVERAGETRADEDPRICE,
+            },
+          });
         }
       } else {
-        callback(null, { list: { status: "0", message: "Data not find" } });
+        callback(null, {
+          StrikePrice: { status: "0", message: "Data not find", value: 0 },
+        });
       }
     });
   };
+  TdDerivatives.productexpirydate = (type, callback) => {
+    const currenturl = `${configt.stock.connector}/GetExpiryDates/?accessKey=${configt.stock.key}&exchange=NFO&product=${type}`;
+    request(currenturl, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        console.log(currenturl);
+        if (_.isEmpty(body)) {
+          callback(null, {
+            List: { status: "0", message: "Data not find" },
+          });
+        } else {
+          const expApiResult = JSON.parse(body);
+          console.log(currenturl);
+          let selectedDate = expApiResult.EXPIRYDATES[0];
+          callback(null, {
+            list: expApiResult.EXPIRYDATES,
+            today: selectedDate,
+          });
+        }
+      } else {
+        callback(null, {
+          List: { status: "0", message: "Data not find" },
+        });
+      }
+    });
+  };
+  TdDerivatives.optiondata = (type, expairdate, strickPrice, callback) => {
+    const currenturl = `${configt.stock.connector}/GetLastQuoteOptionChain/?accessKey=${configt.stock.key}&exchange=NFO&product=${type}&expiry=${expairdate}`;
+    request(currenturl, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        //console.log(currenturl);
+        if (_.isEmpty(body)) {
+          callback(null, {
+            List: { status: "0", message: "Data not find" },
+          });
+        } else {
+          const expApiResult = JSON.parse(body);
+          const apiResult = expApiResult;
+          const putArr = [];
+          const callArr = [];
+          for (const result of apiResult) {
+            const identi = result.INSTRUMENTIDENTIFIER.split("_");
+            const value = parseInt(identi[4]);
+            if (result.SERVERTIME > 0) {
+              if (identi[3] === "CE") {
+                callArr.push({
+                  ...result,
+                  value,
+                  optionType: identi[3],
+                  optionDate: identi[2],
+                });
+              } else if (identi[3] === "PE") {
+                putArr.push({
+                  ...result,
+                  value,
+                  optionType: identi[3],
+                  optionDate: identi[2],
+                });
+              }
+            }
+          }
+
+          const currentOptionStrike = strickPrice;
+          const result = findClosestItem(callArr, currentOptionStrike, "value");
+          const index = result.index;
+          const nearestValue = result.nearestValue;
+          if (index !== -1) {
+            const dataList = [];
+            for (let i = index - 6; i < index + 7; i++) {
+              dataList.push({
+                put: putArr[i],
+                call: callArr[i],
+              });
+            }
+            callback(null, {
+              list: dataList,
+              strike: nearestValue,
+            });
+          }
+        }
+      } else {
+        callback(null, {
+          List: { status: "0", message: "Data not find" },
+        });
+      }
+    });
+  };
+  TdDerivatives.optionChartdata = (type, expairdate, strickPrice, callback) => {
+    const currenturl = `${configt.stock.connector}/GetLastQuoteOptionChain/?accessKey=${configt.stock.key}&exchange=NFO&product=${type}&expiry=${expairdate}`;
+    request(currenturl, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        //console.log(currenturl);
+        if (_.isEmpty(body)) {
+          callback(null, {
+            List: { status: "0", message: "Data not find" },
+          });
+        } else {
+          const expApiResult = JSON.parse(body);
+          const apiResult = expApiResult;
+          const putArr = [];
+          const callArr = [];
+          for (const result of apiResult) {
+            const identi = result.INSTRUMENTIDENTIFIER.split("_");
+            const value = parseInt(identi[4]);
+            if (result.SERVERTIME > 0) {
+              if (identi[3] === "CE") {
+                callArr.push({
+                  ...result,
+                  value,
+                  optionType: identi[3],
+                  optionDate: identi[2],
+                });
+              } else if (identi[3] === "PE") {
+                putArr.push({
+                  ...result,
+                  value,
+                  optionType: identi[3],
+                  optionDate: identi[2],
+                });
+              }
+            }
+          }
+
+          const currentOptionStrike = strickPrice;
+          const result = findClosestItem(callArr, currentOptionStrike, "value");
+          const index = result.index;
+          const nearestValue = result.nearestValue;
+          if (index !== -1) {
+            const dataList = [];
+            for (let i = index - 6; i < index + 7; i++) {
+              dataList.push({
+                put: putArr[i]["OPENINTERESTCHANGE"],
+                call: callArr[i]["OPENINTERESTCHANGE"],
+                putoi: putArr[i]["OPENINTEREST"],
+                calloi: callArr[i]["OPENINTEREST"],
+                label: callArr[i]["value"],
+              });
+            }
+            callback(null, {
+              list: dataList,
+              strike: nearestValue,
+            });
+          }
+        }
+      } else {
+        callback(null, {
+          List: { status: "0", message: "Data not find" },
+        });
+      }
+    });
+  };
+  // Define a function to find the closest item in an array
+  function findClosestItem(arr, value, key) {
+    let closest = null;
+    let index = -1;
+    let nearestValue = null;
+    arr.forEach((item, i) => {
+      if (item[key] > 0) {
+        const diff = Math.abs(value - item.value);
+        if (closest === null || diff < closest) {
+          closest = diff;
+          nearestValue = item.value; // Update the nearest value
+          index = i;
+        }
+      }
+    });
+    return { closest, index, nearestValue };
+  }
 };
--- TS-frontend/src/pages/Authentication/Register.js
+++ TS-frontend/src/pages/Authentication/Register.js
@@ -1,75 +1,82 @@
-import React, { useEffect } from "react";
-import { Row, Col, CardBody, Card, Alert, Container, Input, Label, Form, FormFeedback } from "reactstrap";
+import React, { useEffect } from "react"
+import {
+  Row,
+  Col,
+  CardBody,
+  Card,
+  Alert,
+  Container,
+  Input,
+  Label,
+  Form,
+  FormFeedback,
+} from "reactstrap"
 
 // Formik Validation
-import * as Yup from "yup";
-import { useFormik } from "formik";
+import * as Yup from "yup"
+import { useFormik } from "formik"
 
 // action
-import { registerUser, apiError } from "../../store/actions";
+import { registerUser, apiError } from "../../store/actions"
 
 //redux
-import { useSelector, useDispatch } from "react-redux";
-import { createSelector } from "reselect";
+import { useSelector, useDispatch } from "react-redux"
+import { createSelector } from "reselect"
 
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom"
 
 // import images
-import profileImg from "../../assets/images/profile-img.png";
-import logoImg from "../../assets/images/logo.svg";
+import profileImg from "../../assets/images/profile-img.png"
+import logoImg from "../../assets/images/logo.svg"
 
 const Register = props => {
-
   //meta title
-  document.title = "Register | Skote - React Admin & Dashboard Template";
+  document.title = "Register | Skote - React Admin & Dashboard Template"
 
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
 
   const validation = useFormik({
     // enableReinitialize : use this flag when initial values needs to be changed
     enableReinitialize: true,
 
     initialValues: {
-      email: '',
-      username: '',
-      password: '',
+      email: "",
+      username: "",
+      password: "",
     },
     validationSchema: Yup.object({
       email: Yup.string().required("Please Enter Your Email"),
       username: Yup.string().required("Please Enter Your Username"),
       password: Yup.string().required("Please Enter Your Password"),
     }),
-    onSubmit: (values) => {
-      dispatch(registerUser(values));
-    }
-  });
+    onSubmit: values => {
+      dispatch(registerUser(values))
+    },
+  })
 
-
-  const selectAccountState = (state) => state.Account;
-  const AccountProperties = createSelector(
-    selectAccountState,
-    (account) => ({
-      user: account.user,
-      registrationError: account.registrationError,
-      success: account.success
-      // loading: account.loading,
-    })
-  );
+  const selectAccountState = state => state.Account
+  const AccountProperties = createSelector(selectAccountState, account => ({
+    user: account.user,
+    registrationError: account.registrationError,
+    success: account.success,
+    // loading: account.loading,
+  }))
 
   const {
     user,
-    registrationError, success
+    registrationError,
+    success,
     // loading
-  } = useSelector(AccountProperties);
+  } = useSelector(AccountProperties)
 
   useEffect(() => {
-    dispatch(apiError(""));
-  }, []);
+    dispatch(apiError(""))
+  }, [])
 
-useEffect(() => {
-  success && setTimeout(() => navigate("/login"), 2000)
-}, [success])
+  useEffect(() => {
+    success && setTimeout(() => navigate("/login"), 2000)
+  }, [success])
 
   return (
     <React.Fragment>
@@ -114,10 +121,10 @@ useEffect(() => {
                   <div className="p-2">
                     <Form
                       className="form-horizontal"
-                      onSubmit={(e) => {
-                        e.preventDefault();
-                        validation.handleSubmit();
-                        return false;
+                      onSubmit={e => {
+                        e.preventDefault()
+                        validation.handleSubmit()
+                        return false
                       }}
                     >
                       {user && user ? (
@@ -142,11 +149,15 @@ useEffect(() => {
                           onBlur={validation.handleBlur}
                           value={validation.values.email || ""}
                           invalid={
-                            validation.touched.email && validation.errors.email ? true : false
+                            validation.touched.email && validation.errors.email
+                              ? true
+                              : false
                           }
                         />
                         {validation.touched.email && validation.errors.email ? (
-                          <FormFeedback type="invalid">{validation.errors.email}</FormFeedback>
+                          <FormFeedback type="invalid">
+                            {validation.errors.email}
+                          </FormFeedback>
                         ) : null}
                       </div>
 
@@ -160,11 +171,17 @@ useEffect(() => {
                           onBlur={validation.handleBlur}
                           value={validation.values.username || ""}
                           invalid={
-                            validation.touched.username && validation.errors.username ? true : false
+                            validation.touched.username &&
+                            validation.errors.username
+                              ? true
+                              : false
                           }
                         />
-                        {validation.touched.username && validation.errors.username ? (
-                          <FormFeedback type="invalid">{validation.errors.username}</FormFeedback>
+                        {validation.touched.username &&
+                        validation.errors.username ? (
+                          <FormFeedback type="invalid">
+                            {validation.errors.username}
+                          </FormFeedback>
                         ) : null}
                       </div>
                       <div className="mb-3">
@@ -177,11 +194,17 @@ useEffect(() => {
                           onBlur={validation.handleBlur}
                           value={validation.values.password || ""}
                           invalid={
-                            validation.touched.password && validation.errors.password ? true : false
+                            validation.touched.password &&
+                            validation.errors.password
+                              ? true
+                              : false
                           }
                         />
-                        {validation.touched.password && validation.errors.password ? (
-                          <FormFeedback type="invalid">{validation.errors.password}</FormFeedback>
+                        {validation.touched.password &&
+                        validation.errors.password ? (
+                          <FormFeedback type="invalid">
+                            {validation.errors.password}
+                          </FormFeedback>
                         ) : null}
                       </div>
 
@@ -224,7 +247,7 @@ useEffect(() => {
         </Container>
       </div>
     </React.Fragment>
-  );
-};
+  )
+}
 
-export default Register;
+export default Register
